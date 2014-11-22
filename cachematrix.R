b58299c4c4fb2f70ad17c545cac64edafb65611f
fingerbun <fingerbun@westnet.com.au>
2014-11-22 22:35:06 +1100

    First cut of my assignmetn file. No code yet
    :x

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+###########################################################
+## R PRogramming MOOC Assigment 2
+## See Readme for more info
+###########################################################
 
-## Write a short comment describing this function
+    
+##----------------------------------------------------------
+##   Name :         makeCacheMatrix
+##   Args :
+##   Description :  Creates a special "matrix" object that
+##                  can cache its inverse.
+##----------------------------------------------------------
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -9,6 +17,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+##----------------------------------------------------------
+##   Name :         cacheSolve
+##   Args :
+##   Description :  If the inverse of the matrix returned by
+##                  `makeCacheMatrix`has already been calculated 
+##                  and has not changed), retrieve from cache. 
+##                  Otherwise, calculate it.
+##----------------------------------------------------------
+
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
